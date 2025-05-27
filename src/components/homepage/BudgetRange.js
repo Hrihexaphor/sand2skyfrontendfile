61fdb54b0af8a9a5f98641d527afa4f4401e4199
Hrihexaphor <hritesh.hexaphor@gmail.com>
2025-05-27 18:21:42 +0530

    card size changes in budgetrange

--- src/components/homepage/BudgetRange.js
+++ src/components/homepage/BudgetRange.js
@@ -87,7 +87,7 @@ const BudgetRange = () => {
   slidesToShow: filtered.length === 1 ? 1 : 3,
   slidesToScroll: 1,
   centerMode: filtered.length === 1,
-  centerPadding: filtered.length === 1 ? '38%' : '0px',
+  centerPadding: filtered.length === 1 ? '30%' : '0px',
           responsive: [
             {
               breakpoint: 1024,
